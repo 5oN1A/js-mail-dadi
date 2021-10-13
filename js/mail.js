ef32bdac6c9d1845c050ae967aa9f42fe01a08e9
5oN1A <guarino.sonia@gmail.com>
2021-10-13 15:37:17 +0200

    mail complete

--- js/mail.js
+++ js/mail.js
@@ -1,9 +1,35 @@
 /*
 1.creare array con l'elenco degli indirizzi email di chi può accedere
-2.creare un prompt in cui l'utente inserisce l'indirzzo mail per la verifica
-3.controllo del prompt (i valori devono essere lowcase)
+2.creare un prompt in cui l'utente inserisce l'indirzzo mail per la verifica (i valori devono essere lowcase)
 4.creazione del ciclo per il controllo
 5.se l'indirizzo mail è presente nell'elenco degli indirizzi email di chi può accedere --> alert: puoi accedere
   se l'indirizzo mail non è presente nell'elenco degli indirizzi email di chi può accedere --> alert: non puoi accedere
 
  */
+
+const usersList = ["banana@gmail.it", "peach@hotmail.com", "papaya@yahoo.it", "orange@email.com", "strawberry@fruit.it"];
+
+const userEmail = prompt("inserisci il tuo indirizzo e-mail:").toLowerCase();
+
+console.log(userEmail);
+
+let userConfirmed = false;
+
+for (let index = 0; index < usersList.length; index++) {
+    const userCheck = usersList[index];
+
+   // console.log(userCheck);
+
+   if (userEmail === userCheck) {
+    userConfirmed = true;
+   }
+}
+
+if (!userConfirmed) {
+    alert("Non puoi accedere");
+} else {
+    alert("Puoi accedere");
+}
+
+
+
