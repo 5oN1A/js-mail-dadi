61d610fc69723c1157da8063cf4e9ff9b3a7481e
5oN1A <guarino.sonia@gmail.com>
2021-10-13 14:33:22 +0200

    files setting + mail human-code

---
+++ js/mail.js
@@ -0,0 +1,9 @@
+/*
+1.creare array con l'elenco degli indirizzi email di chi può accedere
+2.creare un prompt in cui l'utente inserisce l'indirzzo mail per la verifica
+3.controllo del prompt (i valori devono essere lowcase)
+4.creazione del ciclo per il controllo
+5.se l'indirizzo mail è presente nell'elenco degli indirizzi email di chi può accedere --> alert: puoi accedere
+  se l'indirizzo mail non è presente nell'elenco degli indirizzi email di chi può accedere --> alert: non puoi accedere
+
+ */
